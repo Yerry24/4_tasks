fa8ee3591f96ad3f2fd2c59686b2890a92531e33
Yerry24 <162577993+Yerry24@users.noreply.github.com>
2024-11-11 15:12:58 +0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+count_of_works = 12
+count_of_hours = 1.5
+name_of_course = 'Python'
+time_on_work =  count_of_hours / count_of_works
+print('Курс:',name_of_course,', всего задач:',count_of_works,', затрачено часов:',count_of_hours,', среднее время выполнения',time_on_work,'часа.')
